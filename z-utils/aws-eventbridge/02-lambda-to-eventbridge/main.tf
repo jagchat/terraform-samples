1122a58279b9bc311b8212a5d31afaa0809a5eb4
Jag <jag@Jags-Mac-mini.attlocal.net>
2023-02-10 09:33:20 -0800

    one sample added

---
+++ z-utils/aws-eventbridge/02-lambda-to-eventbridge/main.tf
@@ -0,0 +1,23 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region     = "us-east-2"
+  access_key = ""
+  secret_key = ""
+  token      = "" #for MFA
+}
+
